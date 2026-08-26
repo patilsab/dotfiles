a619c28ddbf196764589df6acc237f8f20f1a6b6
Rupesh Patil <guru_error_red@yahoo.com>
2026-08-26 22:40:59 +0530

    Create polybar_launch.sh to start Polybar
    Added a script to launch Polybar with logging.

---
+++ .config/polybar/polybar_launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1
+echo "---" | tee -a /tmp/polybar.log
+polybar bar1 -c /home/kali/.config/polybar/polybar.ini 2>&1 | tee -a /tmp/polybar.log &
+disown
+
+echo "Bars launched..."
